bc45418f1bbcb21d9454eb471e46c9301177eb0b
KellyYang <housemeow@gmail.com>
2018-02-24 03:06:54 +0800

    cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import to from 'await-to-js'
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
