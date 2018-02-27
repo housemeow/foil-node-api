9abc3dc43f195b6a8de2ac34c0c9102a8e2bc2b7
KellyYang <housemeow@gmail.com>
2018-02-27 14:43:05 +0800

    抽出參數

--- src/restRoute.js
+++ src/restRoute.js
@@ -2,53 +2,54 @@ const express = require('express');
 
 function restRouter(app, option) {
   const router = express.Router();
+  const { model, path } = option;
 
   router.get('', async (req, res) => {
-    const [err, models] = await option.model.list();
-    if(models) {
-      res.send(models);
+    const [err, items] = await model.list();
+    if(items) {
+      res.send(items);
     } else {
       res.status(err.statusCode).send(err.detail);
     }
   });
 
   router.post('', async (req, res) => {
-    const [err, model] = await option.model.add(req.body);
-    if(model) {
-      res.send(model);
+    const [err, item] = await model.add(req.body);
+    if(item) {
+      res.send(item);
     } else {
       res.status(err.statusCode).send(err.detail);
     }
   });
 
   router.get('/:id', async (req, res) => {
-    const [err, model] = await option.model.get(req.params.id);
-    if(model) {
-      res.send(model);
+    const [err, item] = await model.get(req.params.id);
+    if(item) {
+      res.send(item);
     } else {
       res.status(err.statusCode).send(err.detail);
     }
   });
 
   router.put('/:id', async (req, res) => {
-    const [err, model] = await option.model.update(req.params.id, req.body);
-    if(model) {
-      res.send(model)
+    const [err, item] = await model.update(req.params.id, req.body);
+    if(item) {
+      res.send(item)
     } else {
       res.status(err.statusCode).send(err.detail);
     }
   });
 
   router.delete('/:id', async (req, res) => {
-    const [err, model] = await option.model.delete(req.params.id);
-    if(model) {
-      res.send(model)
+    const [err, item] = await model.delete(req.params.id);
+    if(item) {
+      res.send(item)
     } else {
       res.status(err.statusCode).send(err.detail);
     }
   })
 
-  app.use(option.path, router);
+  app.use(path, router);
 }
 
 export default restRouter;
